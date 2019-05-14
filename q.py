88f247aa47db933a9775c4076e8a60f95d13d844
OxQwerty <33261290+OxQwerty@users.noreply.github.com>
2019-05-14 13:52:29 +0300

    Add files via upload

---
+++ q.py
@@ -0,0 +1,9 @@
+import telebot
+
+bot = telebot.TeleBot("870336394:AAE_gfsjKE9AN6STu0VbQm1gSyS8vpW6YkU")
+
+@bot.message_handler(content_types=['text'])
+def send_echo(message):
+	bot.reply_to(message, message.text)
+	
+bot.polling( none_stop = True )
