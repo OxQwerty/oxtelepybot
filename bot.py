5cbcf0829f10d361c7e4a78e3e7785f9f342f349
OxQwerty <33261290+OxQwerty@users.noreply.github.com>
2019-05-14 17:47:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,4 +6,4 @@ bot = telebot.TeleBot("870336394:AAE_gfsjKE9AN6STu0VbQm1gSyS8vpW6YkU")
 def send_echo(message):
 	bot.reply_to(message, message.text)
 	
-bot.polling( none_stop = True )
+bot.polling(none_stop = True)
