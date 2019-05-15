8686eaecdba54814fdcc8991b98fca7b3e93d595
OxQwerty <33261290+OxQwerty@users.noreply.github.com>
2019-05-15 13:02:34 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,10 @@
+import telebot
 
+bot = telebot.TeleBot("TOKEN")
+
+@bot.message_handler(content_types=['text'])
+def send_echo(message):
+	#bot.reply_to(message, message.text)
+	bot.send_message(message.chat.id, message.text)
+
+bot.polling()
